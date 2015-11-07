e20a9dc5f32e55d908daa1678d451bbdb60e759e
Casey Ann Cumbow <cumbow8@gmail.com>
2015-11-07 14:49:41 -0600

    updates user migration and adds seed data for users

--- db/migrate/20151107201134_create_users.rb
+++ db/migrate/20151107201134_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :bio
+      t.string :password_digest
+      t.string :location
 
       t.timestamps
     end
